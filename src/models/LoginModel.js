e0166456dc2e66c300e2d041f7163a1ade672ec7
ErickyGomes <ericky.gomes@yahoo.com>
2021-04-21 10:59:45 -0300

    update

---
+++ src/models/LoginModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+  constructor(body){
+    this.body = body;
+  }
+}
+
+module.exports = Login;
