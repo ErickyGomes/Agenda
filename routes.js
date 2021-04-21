24243586632c43119bab9fa938511d10cde6196b
ErickyGomes <ericky.gomes@yahoo.com>
2021-04-21 20:28:16 -0300

    atualização

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ route.get('/', homeController.index);
 // Rotas da login
 route.get('/login', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/register', loginController.login);
 
 
 module.exports = route;
