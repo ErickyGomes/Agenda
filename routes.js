79aca343429acf3729a7f22b5682248296b064fe
ErickyGomes <ericky.gomes@yahoo.com>
2021-04-24 13:56:08 -0300

    atualizado

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
@@ -10,6 +11,8 @@ route.get('/', homeController.index);
 // Rotas do Login
 route.get('/login', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 
 module.exports = route;
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,20 +1,14 @@
-exports.index = (req, res) => {
-  res.render('index');
-};
-
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if (req.session.user) return res.render('login-logado'); 
+  return res.render('login');
 };
 
-exports.register = async (req, res) =>{
-  
+exports.register = async (req, res) =>{ 
   try {
-    
     const login = new Login(req.body);
     await login.register();
-    
     if (login.errors.length > 0) {
       req.flash('errors', login.errors);
       req.session.save(() =>{
@@ -22,18 +16,42 @@ exports.register = async (req, res) =>{
       });
       return;
     }
-
     req.flash('success', 'Seu usuário foi criado com sucesso.');
     req.session.save(() =>{
       return res.redirect('/login');
     });
-  
     return res.send(login.success);
-
   } catch (e) {
       console.log(e);
       return res.render('404');
   }
-  
- 
+}
+
+exports.login = async (req, res) =>{ 
+  try {
+    const login = new Login(req.body);
+    await login.login();
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() =>{
+        return res.redirect('/login');
+      });
+      return;
+    }
+    
+    req.flash('success', 'Logado com sucesso.');
+    req.session.user = login.user;
+    req.session.save(() =>{
+      return res.redirect('/login');
+    });
+    return res.send(login.success);
+  } catch (e) {
+      console.log(e);
+      return res.render('404');
+  }
+}
+
+exports.logout = (req,res) => {
+  req.session.destroy();
+  res.redirect('/');
 }
